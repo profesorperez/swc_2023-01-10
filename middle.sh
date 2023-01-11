d2f2426e6dfded09f547bb20e013572ee7439a2d
Felipe Perez <felipe.perez@ou.edu>
2023-01-11 16:32:47 +0000

    added comments and allowed for passing head size and number of middle lines

--- middle.sh
+++ middle.sh
@@ -1,1 +1,10 @@
-head -n 75 $1 | tail -n 5
+# script to list the middle of a file
+#
+# $1 : file name
+# $2 : number of head lines
+# $3 : 
+#
+# usage:
+# bash middle.sh <file>
+
+head -n $2 $1 | tail -n $3
