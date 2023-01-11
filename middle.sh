bfb6cdd23aac8539f03b362c6c722fac79bacb2e
Felipe Perez <felipe.perez@ou.edu>
2023-01-11 16:56:08 +0000

    expanded documentation

--- middle.sh
+++ middle.sh
@@ -2,9 +2,11 @@
 #
 # $1 : file name
 # $2 : number of head lines
-# $3 : 
+# $3 : number of tail lines
 #
 # usage:
-# bash middle.sh <file>
+# bash middle.sh [filename] [number of head lines] [number of tail lines]
+# example:
+# bash middle.sh minotaur.dat 50 5
 
 head -n $2 $1 | tail -n $3
